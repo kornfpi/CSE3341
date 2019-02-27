ac5210d4e707511184ac9337161285e29c0c9b30
John <wolford.72@osu.edu>
2019-02-27 17:18:35 -0500

    Symbol Table

--- Parser/src/Test.java
+++ Parser/src/Test.java
@@ -2,9 +2,13 @@
 public class Test {
     
     public static void main(String[] args) {
+
+
         Tokenizer tokenizer;
+  
         if(args.length != 0) {
             tokenizer = new Tokenizer(args[0]);
+
             //tokenizer.printParseValues();
             Nodes parser = new Nodes(tokenizer);
             parser.parseTokens();
